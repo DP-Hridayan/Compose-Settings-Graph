344e5fdb3ef014ce72563740dbc110217efad031
DP-Hridayan <hridayanofficial@gmail.com>
2026-09-10 18:51:21 +0530

    Allow custom onClick handler for switch items

--- settingsgraph/build.gradle.kts
+++ settingsgraph/build.gradle.kts
@@ -44,7 +44,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.dp-hridayan",
         artifactId = "compose-settings-graph",
-        version = "1.0.0"
+        version = "1.1.0"
     )
 
     pom {
